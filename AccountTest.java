9813efc331260be3bfd7e6616fe4b339d1e9ace1
Janeeta Samahat Nowa <janeeta_nowa_34rpl@student.smktelkom-mlg.sch.id>
2025-08-20 21:06:50 +0700

    Add files via upload

---
+++ AccountTest.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        SavingsAccount sa0001 = new SavingsAccount();
+        sa0001.saldo = 1000;
+        sa0001.name = "Jane";
+        sa0001.bunga = 0.02;
+        
+        SavingsAccount sa0002 = new SavingsAccount();
+        sa0002.saldo = 2000;
+        sa0002.name = "Jasmine";
+        sa0002.bunga  = 0.015;
+
+        System.out.println("------ Nasabah jane cantik imut ------");
+        sa0001.displayCustomer();
+        System.out.println("");
+        System.out.println("------ Nasabah jasmine gemoi ------");
+        sa0002.displayCustomer();
+
+        System.out.println(" ");
+        
+        System.out.println("------ Transaksi jane ------");
+        sa0001.displayCustomer();
+        System.out.print("mau setor(1) atau tarik(2)? ");//saya mau nambahkan inputan setor atau tarik buat variasi aja buk :)
+        int pilihan1 = input.nextInt();
+        if (pilihan1 == 1){
+            System.out.print("jumlah setor : ");
+            double setor = input.nextDouble();
+            sa0001.deposit(setor);
+        }else if (pilihan1 == 2){
+            System.out.print("jumlah penarikan : ");
+            double tarik = input.nextDouble();
+            sa0001.withdraw(tarik);
+        }else {
+            System.out.println("error : masukkan piihan yang benar");
+            System.exit(0);
+        }
+        
+        
+        sa0001.saldo += sa0001.calInterest();
+        System.out.println("saldo setelah bunga : " + sa0001.saldo);
+
+        System.out.println(" ");
+        System.out.println("------ Transaksi jasmine ------");
+        sa0002.displayCustomer();
+        System.out.print("mau setor(1) atau tarik(2)? ");
+        int pilihan2 = input.nextInt();
+        if (pilihan2== 1){
+            System.out.print("jumlah setor : ");
+            int setor = input.nextInt();
+            sa0002.deposit(setor);
+        }else if (pilihan2 == 2){
+            System.out.print("jumlah penarikan : ");
+            int tarik = input.nextInt();
+            sa0002.withdraw(tarik);
+        }else{
+            System.out.println("error : masukkan pilihan yang benar");
+            System.exit(0);
+        }
+        
+        sa0002.saldo += sa0002.calInterest();
+        System.out.println("saldo setelah bunga : " + sa0002.saldo);
+
+
+    
+}
+}
---
+++ SavingsAccount.java
@@ -0,0 +1,34 @@
+public class SavingsAccount {
+    public double saldo;
+    public double bunga = 0.01;
+    public String name;
+
+    public void displayCustomer(){
+        System.out.println("Nasabah   : "+ name);
+        System.out.println("Saldo     : "+saldo);
+        System.out.println("Bunga     : "+ bunga);
+    }
+    
+    public void deposit(double setor){
+        saldo += setor;
+        System.out.println("berhasil menyetor : "+ setor +", saldo baru : "+ saldo);
+    }
+
+    public void withdraw(double amount) {
+        if (amount < 0){
+            System.out.println("jumlah penarikan tidak boleh negatif");
+        } else if (amount > saldo){
+            System.out.println("saldo tidak cukup untuk penarikan");
+        } else {
+            saldo -= amount;
+            System.out.println("berhasil menarik : "+ amount +", saldo baru : "+ saldo);
+        }
+    }
+
+    public double calInterest(){
+        double interest = saldo * bunga/12;
+        return interest;
+    }
+        
+}
+
